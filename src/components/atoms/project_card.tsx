846b62bf08013be9a094e31abb19cc76b9572d7a
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-03-23 09:22:41 +0530

    fix: project_card.tsx

--- src/components/atoms/project_card.tsx
+++ src/components/atoms/project_card.tsx
@@ -38,8 +38,8 @@ export default function ProjectCard({
           />
           <div className="mt-2 flex items-center flex-col">
             <ul className="flex space-x-4  text-xs border-gray-800 w-full py-1 my-1 items-center justify-center text-gray-600">
-              {techStack.map((tech) => (
-                <li>{tech}</li>
+              {techStack.map((tech: any) => (
+                <li key={tech}>{tech}</li>
               ))}
             </ul>
             <h1 className="text-center text-xl uppercase font-bold text-white">
